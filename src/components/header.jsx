4d1d13f9e66e7093a61466af0c24353f82b0e03a
A.L.U.Priyashantha <lahiruu432@gmail.com>
2025-05-09 21:18:23 +0530

    product card edit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return(
-    <header className="w-full bg-blue-500 h-[50px]">
-      <h1>My Store</h1>
+    <header className="w-full h-[70px] flex justify-center items-center bg-gray-200">
+      <div className=" w-[500px] h-full flex justify-evenly items-center text-pink-400 text-xl">
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/contact">Contact us</Link>
+          <Link to="/rewiews">Reviews</Link>
+      </div>
       
     </header>
   )
